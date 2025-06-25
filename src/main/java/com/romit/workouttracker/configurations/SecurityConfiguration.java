6a868da6f2773025f3a0b99f709555d0cd52b3fb
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2025-06-26 03:34:36 +0530

    removed unused imports

--- src/main/java/com/romit/workouttracker/configurations/SecurityConfiguration.java
+++ src/main/java/com/romit/workouttracker/configurations/SecurityConfiguration.java
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration {
